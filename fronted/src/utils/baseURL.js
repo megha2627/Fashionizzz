cc7fc4c5055525fdb54503caed06fa1a98066cf3
Megha Gupta <155795205+megha2627@users.noreply.github.com>
2025-07-23 12:22:34 +0530

    Update baseURL.js

--- fronted/src/utils/baseURL.js
+++ fronted/src/utils/baseURL.js
@@ -1,5 +1,8 @@
-export const getBaseUrl = () => {
+/*export const getBaseUrl = () => {
     //return "http://localhost:3000"
     //return "https://fashionizzz.onrender.com"
      return "http://fashionizzz-6.onrender.com"
+}*/
+export const getBaseUrl = () => {
+    return import.meta.env.VITE_API_URL;
 }
