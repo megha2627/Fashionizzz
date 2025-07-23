a85dfd3ded4cf28abb8aa14a96b646810629efd0
Megha Gupta <155795205+megha2627@users.noreply.github.com>
2025-07-23 11:49:11 +0530

    Update baseURL.js

--- fronted/src/utils/baseURL.js
+++ fronted/src/utils/baseURL.js
@@ -1,5 +1,5 @@
 export const getBaseUrl = () => {
     //return "http://localhost:3000"
     //return "https://fashionizzz.onrender.com"
-     return "https://fashionizzz-6.onrender.com"
+     return "http://fashionizzz-6.onrender.com"
 }
